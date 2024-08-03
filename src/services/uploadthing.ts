82bcaae477fe7b9310935d005943cdc2456d8519
Adel Rodríguez <hello@adelrodriguez.com>
2024-08-03 15:27:05 -0400

    refactor: add uploadthing api export

--- src/services/uploadthing.ts
+++ src/services/uploadthing.ts
@@ -1,38 +1,41 @@
+"server-only"
+
 import { validateRequest } from "@/lib/auth"
+import rateLimiter from "@/lib/rate-limit"
 import {
   type FileRouter as UploadThingFileRouter,
   createUploadthing,
 } from "uploadthing/next"
 import { UploadThingError } from "uploadthing/server"
+import { UTApi } from "uploadthing/server"
+
+export const utapi = new UTApi()
 
 const router = createUploadthing()
 
-const auth = (req: Request) => ({ id: "fakeId" }) // Fake auth function
-
-// FileRouter for your app, can contain multiple FileRoutes
 export const fileRouter = {
-  // Define as many FileRoutes as you like, each with a unique routeSlug
-  imageUploader: router({ image: { maxFileSize: "32MB" } })
+  imageUploader: router({ image: { maxFileSize: "4MB" } })
     // Set permissions and file types for this FileRoute
     .middleware(async ({ req }) => {
-      // This code runs on your server before upload
       const { user } = await validateRequest()
 
-      // If you throw, the user will not be able to upload
       if (!user) throw new UploadThingError("Unauthorized")
 
-      // Whatever is returned here is accessible in onUploadComplete as `metadata`
+      const limit = await rateLimiter.user.limit(user.email)
+
+      if (!limit.success) {
+        throw new UploadThingError("Rate limit exceeded")
+      }
+
       return { userId: user.id }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      // This code RUNS ON YOUR SERVER after upload
       console.log("Upload complete for userId:", metadata.userId)
 
-      console.log("file url", file.url)
+      console.log("File URL:", file.url)
 
       return { uploadedBy: metadata.userId }
     }),
 } satisfies UploadThingFileRouter
 
 export type FileRouter = typeof fileRouter
-export default fileRouter
