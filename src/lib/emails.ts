e87c00561c1646aad40e6b952d560a283557e1db
Adel Rodríguez <hello@adelrodriguez.com>
2024-08-07 19:08:48 -0400

    refactor: improve email mock logging

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -4,7 +4,9 @@ import { MOCK_RESEND_EMAIL } from "@/lib/consts"
 import env from "@/lib/env.server"
 import resend from "@/services/resend"
 import { SendEmailError } from "@/utils/error"
+import log from "@/utils/log"
 import { render } from "@react-email/render"
+import chalk from "chalk"
 import type { ReactElement } from "react"
 
 export async function sendEmail(
@@ -13,13 +15,16 @@ export async function sendEmail(
   body: ReactElement,
 ) {
   if (env.RESEND_API_KEY === MOCK_RESEND_EMAIL) {
-    console.info("MOCKING EMAIL SEND")
-    console.info("Sending email to", email)
+    log.info("MOCKING EMAIL SEND")
+
+    console.info(chalk.bold.blue("Sending email to"), chalk.italic.white(email))
+    log.info("Email content:")
     console.info(
-      "Email content:",
-      render(body, {
-        plainText: true,
-      }),
+      chalk.italic(
+        render(body, {
+          plainText: true,
+        }),
+      ),
     )
 
     return
