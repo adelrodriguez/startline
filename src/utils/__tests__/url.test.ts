0bb48ca51c51946fb8c7d58a0f69debed6776b07
Adel Rodríguez <hello@adelrodriguez.com>
2024-08-19 14:20:54 -0400

    refactor: clean up test

--- src/utils/__tests__/url.test.ts
+++ src/utils/__tests__/url.test.ts
@@ -1,32 +1,38 @@
 import { buildBaseUrl, buildUrl } from "@/utils/url"
-import { expect, test } from "vitest"
+import { describe, expect, test } from "vitest"
 
-test("builds a base url", () => {
-  const url = buildBaseUrl()
+describe("buildBaseUrl", () => {
+  test("builds a base url with the correct protocol", () => {
+    const url = buildBaseUrl("http")
+    const url2 = buildBaseUrl("https")
 
-  expect(url).toBe("https://startline.dev")
+    expect(url).toBe("http://startline.dev")
+    expect(url2).toBe("https://startline.dev")
+  })
 })
 
-test("builds a url", () => {
-  const url = buildUrl("/foo")
+describe("buildUrl", () => {
+  test("builds a url", () => {
+    const url = buildUrl("/foo")
 
-  expect(url).toBe("https://startline.dev/foo")
-})
+    expect(url).toBe("https://startline.dev/foo")
+  })
 
-test("builds a url with a custom protocol", () => {
-  const url = buildUrl("/foo", { protocol: "http" })
+  test("builds a url with a custom protocol", () => {
+    const url = buildUrl("/foo", { protocol: "http" })
 
-  expect(url).toBe("http://startline.dev/foo")
-})
+    expect(url).toBe("http://startline.dev/foo")
+  })
 
-test("builds a url with query params", () => {
-  const url = buildUrl("/foo", { query: { bar: "{baz}" } })
+  test("builds a url with query params", () => {
+    const url = buildUrl("/foo", { query: { bar: "{baz}" } })
 
-  expect(url).toBe("https://startline.dev/foo?bar=%7Bbaz%7D")
-})
+    expect(url).toBe("https://startline.dev/foo?bar=%7Bbaz%7D")
+  })
 
-test("builds a url with decoded query params", () => {
-  const url = buildUrl("/foo", { query: { bar: "{baz}" }, decoded: true })
+  test("builds a url with decoded query params", () => {
+    const url = buildUrl("/foo", { query: { bar: "{baz}" }, decoded: true })
 
-  expect(url).toBe("https://startline.dev/foo?bar={baz}")
+    expect(url).toBe("https://startline.dev/foo?bar={baz}")
+  })
 })
