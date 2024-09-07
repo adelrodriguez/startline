c6048a4f38d91572f60f9aa7539b0b89335f1102
Adel Rodríguez <hello@adelrodriguez.com>
2024-09-07 11:51:08 -0400

    feat: save uploadthing assets

--- src/services/uploadthing.ts
+++ src/services/uploadthing.ts
@@ -6,6 +6,8 @@ import { UploadThingError } from "uploadthing/server"
 import { UTApi } from "uploadthing/server"
 import { validateRequest } from "~/lib/auth"
 import rateLimiter from "~/lib/rate-limit"
+import { createAsset } from "~/server/data/asset"
+import { createUserId } from "~/server/data/user"
 import { RateLimitError } from "~/utils/error"
 
 export const utapi = new UTApi()
@@ -26,12 +28,16 @@ export const fileRouter = {
         throw new RateLimitError("Rate limit exceeded")
       }
 
-      return { userId: user.id }
+      return { userId: createUserId(user.id) }
     })
     .onUploadComplete(async ({ metadata, file }) => {
-      console.log("Upload complete for userId:", metadata.userId)
-
-      console.log("File URL:", file.url)
+      await createAsset(metadata.userId, {
+        service: "uploadthing",
+        mimeType: file.type,
+        filename: file.name,
+        size: file.size,
+        url: file.url,
+      })
 
       return { uploadedBy: metadata.userId }
     }),
