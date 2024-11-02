aa880b624013f2be51e212afb5b38cb1b025de80
Adel Rodríguez <hello@adelrodriguez.com>
2024-11-02 00:26:55 -0400

    fix: update stripe api

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -2,7 +2,7 @@ import Stripe from "stripe"
 import env from "~/lib/env.server"
 
 const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-09-30.acacia",
+  apiVersion: "2024-10-28.acacia",
   typescript: true,
 })
 
